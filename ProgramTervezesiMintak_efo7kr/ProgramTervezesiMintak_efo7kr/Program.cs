b3f730dda1c98d064587966484674d787df77ef6
ekozar <eszter.kozar@stud.uni-corvinus.hu>
2021-12-12 15:46:35 +0100

    initial commit

---
+++ ProgramTervezesiMintak_efo7kr/ProgramTervezesiMintak_efo7kr/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramTervezesiMintak_efo7kr
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
